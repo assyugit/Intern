5a47fa8704cfc7c4b2181a7588f4fc2349dec0df
assyugit <assyugit397@gmail.com>
2020-08-14 18:00:42 +0900

    時間差分
    現在時刻と前回の更新時刻との時間差分

--- nowdate.py
+++ nowdate.py
@@ -1,10 +1,13 @@
 #coding:UTF-8
-import datetime
+#import datetime
 from datetime import datetime
 
 dt_now = datetime.now()
 print dt_now
 
-dt_b = "2020-08-09-1506"
-tdate = datetime.strptime(dt_b,'%Y-%m-%d-%H%M')
+dt_p = "2020-08-09-1506"
+tdate = datetime.strptime(dt_p,'%Y-%m-%d-%H%M')
 print tdate
+
+td_m = dt_now - tdate
+print td_m
