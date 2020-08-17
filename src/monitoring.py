a82c874dd6e92d2aaaf924328da7d8293701c93e
assyugit <assyugit397@gmail.com>
2020-08-17 12:49:18 +0900

    課題
    ファイル読み込み　条件判定　時間差分

--- src/monitoring.py
+++ src/monitoring.py
@@ -9,11 +9,9 @@ cnt = 1
 flag = 0
 
 while log_line:
- print str(cnt) + "行目"
- print log_line.strip()
  if cnt==1 or cnt==4:
   if "succeed" in log_line.strip():
-   print "正常終了 : 0"
+   #正常終了 : 0
    if flag==0:
     succes1=0
     flag=1
@@ -21,7 +19,7 @@ while log_line:
     succes2=0
     flag=0
   else:
-   print "以上終了 : 1"
+   #以上終了 : 1
    if flag==0:
     succes1=1
     flag=1
@@ -34,16 +32,15 @@ while log_line:
   pdate = datetime.strptime(dt_p,'%Y-%m-%d-%H%M')
   td_m = dt_now - pdate
   if td_m.days < 7:
-   print "正常終了 : 0"
+   #正常終了 : 0
    time_dif=0
   else:
-   print "以上終了 : 1"
+   #以上終了 : 1
    time_dif=1
 
  log_line=sys.stdin.readline()
  if cnt ==4:
   cnt=0
-  print "結果"
   if succes1==0 and succes2==0 and time_dif==0:
    print "正常終了 : 0"
   else:
