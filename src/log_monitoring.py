a3c48875d12082867c10695b33dc1200c0c04814
assyugit <assyugit397@gmail.com>
2020-08-18 17:51:29 +0900

    課題
    引数オプション処理

--- src/log_monitoring.py
+++ src/log_monitoring.py
@@ -1,6 +1,5 @@
 #coding:UTF-8
 import sys
-import os
 import argparse
 from datetime import datetime
 
@@ -13,13 +12,15 @@ parser.add_argument('-s','--search',help='検索対象文字列',default='succee
 
 args = parser.parse_args()
 
-dt_now = datetime.now()
-sys.stdin = file(args.fname)
-log_line = sys.stdin.readline()
-
-f = open("log_output.txt","w")
-
 try:
+ dt_now = datetime.now()
+ sys.stdin = file(args.fname)
+ log_line = sys.stdin.readline()
+ fresult = None
+ ftime = None
+
+ f = open("log_output.txt","w")
+
  while log_line:
   if args.check in log_line.strip():
    if args.search in log_line.strip():
@@ -39,18 +40,22 @@ try:
    pass
 
   log_line = sys.stdin.readline()
+
+ if fresult is None:
+  out = -4 #最終行が見つからない
+ elif fresult == ftime:
+  out = 0 #正常終了
+ elif fresult == -1 and ftime == 0:
+  out = -1 #バックアップが取れてない
+ elif fresult == 0 and ftime == -2:
+  out = -2 #更新日時が古い
+ else:
+  out = -3 #バックアップ、更新日時、ともに不可
+
+ f.write(str(out))
+ f.close()
+
 except Exception:
  f.write("-9") #予期せぬエラー
  f.close()
-
-if fresult == ftime:
- out = 0 #正常終了
-elif fresult == -1 and ftime == 0:
- out = -1 #バックアップが取れてない
-elif fresult == 0 and ftime == -2:
- out = -2 #更新日時が古い
-else:
- out = -3 #バックアップ、更新日時、ともに不可
-
-f.write(str(out))
-f.close()
+ sys.exit()
